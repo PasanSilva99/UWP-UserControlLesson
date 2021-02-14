877e489ebc4483bcea34f047e82790397e95b807
Pasan Anuththara <pasananuththara19@gmail.com>
2021-02-14 22:42:31 +0530

    Add project files.

---
+++ CodeLyoko/BinINterface.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CodeLyoko
+{
+    public sealed partial class BinINterface : UserControl
+    {
+        //Shadow
+        private Color _shadow;
+
+        public Color CardShadow
+        {
+            get
+            {
+                return _shadow;
+            }
+            set
+            {
+                _shadow = value;
+                shadow.Color = _shadow;
+            }
+        }
+
+        //Border
+        private Color _border;
+
+        public Color CardBorder
+        {
+            get
+            {
+                return _border;
+            }
+            set
+            {
+                _border = value;
+                AcrylicBrush brush = new AcrylicBrush();
+                brush.TintColor = _border;
+                border.Background = brush;
+                btn_call.Background = brush;
+
+            }
+        }
+
+        //BG
+        private Color _bg;
+
+        public Color CardBG
+        {
+            get
+            {
+                return _bg;
+            }
+            set
+            {
+                _bg = value;
+                bg.Background = new SolidColorBrush(_bg);
+            }
+        }
+
+        //TextColor
+        private Color _TextColor;
+
+        public Color TextColor
+        {
+            get
+            {
+                return _TextColor;
+            }
+            set
+            {
+                _TextColor = value;
+                
+            }
+        }
+
+
+        //Text Name
+        private String _lyName = "";
+
+        public String lyName
+        {
+            get
+            {
+                return _lyName;
+            }
+            set
+            {
+                _lyName = value;
+                LyName.Text = _lyName;
+            }
+        }
+
+        //Set Image
+        private String _Image;
+
+        public String CardImage
+        {
+            get
+            {
+                return _Image;
+            }
+            set
+            {
+                _Image = value;
+                card_image.Source = new BitmapImage(new Uri(card_image.BaseUri, _Image));
+            }
+        }
+
+        public BinINterface()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ CodeLyoko/MainPage.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CodeLyoko
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        lyHandler lyHandler = new lyHandler();
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            lyHandler.AddMem(new lyWor { lyName = "Jeramy", Accent = Color.FromArgb((byte)255, (byte)84, (byte)152, (byte)158), Backgound = Color.FromArgb((byte)255, (byte)64, (byte)115, (byte)120), img = "Assets/Jeramy.png" });
+            lyHandler.AddMem(new lyWor { lyName = "Aelita", Accent = Color.FromArgb((byte)255, (byte)153, (byte)41, (byte)158), Backgound = Color.FromArgb((byte)255, (byte)77, (byte)20, (byte)79), img = "Assets/Aelita.jpg" });
+            lyHandler.AddMem(new lyWor { lyName = "Jeramy", Accent = Color.FromArgb((byte)255, (byte)84, (byte)152, (byte)158), Backgound = Color.FromArgb((byte)255, (byte)64, (byte)115, (byte)120), img = "Assets/Jeramy.png" });
+            lyHandler.AddMem(new lyWor { lyName = "Aelita", Accent = Color.FromArgb((byte)255, (byte)153, (byte)41, (byte)158), Backgound = Color.FromArgb((byte)255, (byte)77, (byte)20, (byte)79), img = "Assets/Aelita.jpg" });
+            lyHandler.AddMem(new lyWor { lyName = "Jeramy", Accent = Color.FromArgb((byte)255, (byte)84, (byte)152, (byte)158), Backgound = Color.FromArgb((byte)255, (byte)64, (byte)115, (byte)120), img = "Assets/Jeramy.png" });
+            lyHandler.AddMem(new lyWor { lyName = "Aelita", Accent = Color.FromArgb((byte)255, (byte)153, (byte)41, (byte)158), Backgound = Color.FromArgb((byte)255, (byte)77, (byte)20, (byte)79), img = "Assets/Aelita.jpg" });
+            lyHandler.AddMem(new lyWor { lyName = "Jeramy", Accent = Color.FromArgb((byte)255, (byte)84, (byte)152, (byte)158), Backgound = Color.FromArgb((byte)255, (byte)64, (byte)115, (byte)120), img = "Assets/Jeramy.png" });
+            lyHandler.AddMem(new lyWor { lyName = "Aelita", Accent = Color.FromArgb((byte)255, (byte)153, (byte)41, (byte)158), Backgound = Color.FromArgb((byte)255, (byte)77, (byte)20, (byte)79), img = "Assets/Aelita.jpg" });
+            lyHandler.AddMem(new lyWor { lyName = "Jeramy", Accent = Color.FromArgb((byte)255, (byte)84, (byte)152, (byte)158), Backgound = Color.FromArgb((byte)255, (byte)64, (byte)115, (byte)120), img = "Assets/Jeramy.png" });
+            lyHandler.AddMem(new lyWor { lyName = "Aelita", Accent = Color.FromArgb((byte)255, (byte)153, (byte)41, (byte)158), Backgound = Color.FromArgb((byte)255, (byte)77, (byte)20, (byte)79), img = "Assets/Aelita.jpg" });
+            lyHandler.AddMem(new lyWor { lyName = "Jeramy", Accent = Color.FromArgb((byte)255, (byte)84, (byte)152, (byte)158), Backgound = Color.FromArgb((byte)255, (byte)64, (byte)115, (byte)120), img = "Assets/Jeramy.png" });
+            lyHandler.AddMem(new lyWor { lyName = "Aelita", Accent = Color.FromArgb((byte)255, (byte)153, (byte)41, (byte)158), Backgound = Color.FromArgb((byte)255, (byte)77, (byte)20, (byte)79), img = "Assets/Aelita.jpg" });
+            lyHandler.AddMem(new lyWor { lyName = "Jeramy", Accent = Color.FromArgb((byte)255, (byte)84, (byte)152, (byte)158), Backgound = Color.FromArgb((byte)255, (byte)64, (byte)115, (byte)120), img = "Assets/Jeramy.png" });
+            lyHandler.AddMem(new lyWor { lyName = "Aelita", Accent = Color.FromArgb((byte)255, (byte)153, (byte)41, (byte)158), Backgound = Color.FromArgb((byte)255, (byte)77, (byte)20, (byte)79), img = "Assets/Aelita.jpg" });
+
+            foreach (lyWor w in lyHandler.getAll())
+            {
+                BinINterface iNterface = new BinINterface();
+                iNterface.lyName = w.lyName;
+                iNterface.CardBG = w.Backgound;
+                iNterface.CardBorder = w.Accent;
+                iNterface.CardShadow = w.Accent;
+                iNterface.TextColor = Color.FromArgb((byte)255, (byte)255, (byte)255, (byte)255);
+                iNterface.CardImage = w.img;
+                iNterface.Width = 200.0;
+                iNterface.Height = 300.0;
+
+                stack_profile.Children.Add(iNterface);
+            }
+
+        }
+    }
+
+    public class lyHandler
+    {
+        List<lyWor> lyWors = new List<lyWor>();
+
+        public List<lyWor> getAll()
+        {
+            if(lyWors.Count > 0)
+            {
+                return lyWors;
+            }
+
+            return null;
+        }
+
+        public bool AddMem(lyWor wor)
+        {
+            try
+            {
+                lyWors.Add(wor);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+
+    public class lyWor
+    {
+        public String lyName { get; set; } = "";
+        public String img { get; set; } = "";
+        public Color Accent { get; set; } = Color.FromArgb((byte)255, (byte)255, (byte)255, (byte)255);
+        public Color Backgound { get; set; } = Color.FromArgb((byte)255, (byte)195, (byte)195, (byte)195);
+    }
+
+}
